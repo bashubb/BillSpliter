6158c3a76480c7ccd679fddec38109ef91983f20
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-10-03 11:49:55 +0200

    Initial Commit

---
+++ BillSpliter/BillSpliterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSpliterApp.swift
+//  BillSpliter
+//
+//  Created by HubertMac on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BillSpliterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
