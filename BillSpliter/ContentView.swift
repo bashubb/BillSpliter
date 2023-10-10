c3b6a23e910df30f71a91a95dc1fdce78a1d8d4a
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-10-10 13:26:42 +0200

    ContentView - create  UI protothype

--- BillSpliter/ContentView.swift
+++ BillSpliter/ContentView.swift
@@ -8,17 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var noOfPeople = 2
+    @State private var tipAmount = 20
+    
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Bill Amount", value: $checkAmount, format: .currency(code: "PLN"))
+                    Picker("Number of people", selection: $noOfPeople){
+                        ForEach(0..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section {
+                    Picker("Tip precentage", selection: $tipAmount){
+                        ForEach(0..<51){
+                            Text("\($0)%")
+                        }
+                    }
+                } header: {
+                    Text("HOW MUCH TIP DO YOU WANT TO LEAVE ?")
+                }
+                .pickerStyle(.navigationLink)
+                
+                Section {
+                    Text("Per person")
+                } header: {
+                    Text("AMOUNT PER PERSON")
+                }
+            
+                
+                Section {
+                    Text("total")
+                } header: {
+                    Text("TOTAL WITH TIP")
+                }
+                
+            }
+            .navigationTitle("Bill Spliter")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
